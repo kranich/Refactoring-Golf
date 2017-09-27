4a1a849258ce5897fef8364d99f122a524c07ba2
Andre Kappes <lewwerworscht@vollbio.de>
2017-09-27 13:01:29 +0200

    Add tests for Example2

---
+++ src/Java/0. Warm Up/src/test/refactoringgolf/warmup/begin/Example2ExtractMethodTest.java
@@ -0,0 +1,36 @@
+package refactoringgolf.warmup.begin;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class Example2ExtractMethodTest {
+
+	@Test
+	public void testGetPrice_BasePriceAbove1000() throws Exception {
+		Example2ExtractMethod underTest = new Example2ExtractMethod(10, 200);
+
+		double result = underTest.getPrice();
+
+		assertThat(result, is(2000 * 0.95));
+	}
+
+	@Test
+	public void testGetPrice_BasePriceIs1000() throws Exception {
+		Example2ExtractMethod underTest = new Example2ExtractMethod(10, 100);
+
+		double result = underTest.getPrice();
+
+		assertThat(result, is(1000 * 0.98));
+	}
+
+	@Test
+	public void testGetPrice_QuantityIsZero() throws Exception {
+		Example2ExtractMethod underTest = new Example2ExtractMethod(0, 100);
+
+		double result = underTest.getPrice();
+
+		assertThat(result, is(0.00));
+	}
+}
